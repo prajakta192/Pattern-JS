edb77a6a3c632c134f0f22af6cc61cabaf4729c4
prajakta192 <prajakta.bujare17@gmail.com>
2024-05-29 13:47:20 +0530

    day1 challenge

--- day1/pattern.js
+++ day1/pattern.js
@@ -1,1 +1,15 @@
-alert('hello')
+//square pattern
+
+function pattern_square(n){
+	//run the outer loop = n times
+	for(let row = 1; row <= n; row++){
+		//for every row, run the col
+		for(let col = 1; col <= n; col++){
+			document.write('* ')
+		}
+		//add break after every row
+		document.write('<br/>')
+	}
+}
+
+pattern_square(5)

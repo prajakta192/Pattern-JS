775519811a19b515956f29a64a27245dad9c23a2
prajakta192 <prajakta.bujare17@gmail.com>
2024-05-31 17:11:05 +0530

    triangle pattern2 decresing triangle

--- triangle/pattern.js
+++ triangle/pattern.js
@@ -1,3 +1,7 @@
+function main(n){
+	triPattern_two(n)
+}
+main(5)
 /*increasing triangle pattern 
 
 *	
@@ -21,4 +25,29 @@ function triPattern_one(n){
 		document.write('<br/>')
 	}
 }
-triPattern_one(4)
+//triPattern_one(4)
+
+//decresing triangle pattern
+/*decreasing triangle pattern 
+             n = 5
+* * * * *    row 1 col 1 = 5
+* * * *      row 2 col 2 = 4
+* * *        row 3 col 3 = 3
+* *          row 4 col 4 = 2
+*            row 5 col 5 = 1
+								
+*/
+
+function triPattern_two(n){
+	//run outer loop = n times
+	for(let row = 1; row <= n; row++){
+		//for every row run column
+		for(let col = 1; col <= n - row + 1; col++){
+			document.write('* ')
+
+		}
+		//after every row add break
+		document.write('<br/>')
+	}
+}
+//triPattern_two(5)
